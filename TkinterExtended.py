176fae9f1ec1c914a574cdb8525ae0ed737f77dd
AJLocke1 <133682649+AJLocke1@users.noreply.github.com>
2024-10-05 00:13:22 +0100

    c

---
+++ TkinterExtended.py
@@ -0,0 +1,105 @@
+try:
+    import customtkinter as ctk
+    STACKBASE = ctk.CTkFrame
+    BASECLASS = ctk.CTkBaseClass
+except ModuleNotFoundError:
+    import tkinter as tk
+    STACKBASE = tk.Frame
+    BASECLASS = tk.Widget
+
+class Stack(STACKBASE):
+    """
+    A stack wiget based on the c++ gtkmm4 widget of the same name.
+    This widgetis ideal for setting up multi page applications.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.children_list = []
+        self.visible_child = None
+
+    def add_widget(self, widget: BASECLASS, name: str | None = None) -> None: # type: ignore
+        """
+        adds a widget to the stack
+        """
+        widget.grid(row = 0, column = 0, sticky = "nsew")
+        widget.grid_remove()
+        self.children_list.append({"widget": widget, "name": name})
+
+        if self.visible_child is None:  
+            self.set_visible_child(widget)
+
+    def remove_widget(self, widget_or_identifier: BASECLASS | str) -> None: # type: ignore
+        """
+        removes a widget from the stack
+        """
+        if isinstance(widget_or_identifier, BASECLASS):
+            widget = widget_or_identifier
+            if any(child["widget"] == widget for child in self.children_list):
+                widget.grid_forget()
+                self.children_list.remove(widget)
+
+                if widget == self.visible_child:
+                    if self.children_list:
+                        self.set_visible_child(self.children_list[0]) 
+                    else:
+                        self.visible_child = None
+            else:
+                raise NotInStackError(widget)
+        else:
+            name = widget_or_identifier
+            if any(child["name"] == name for child in self.children_list):
+                for child in self.children_list:
+                    if child["name"] == name:
+                        widget = child["widget"]
+                        widget.grid_forget()
+                        self.children_list.remove(widget)
+
+                        if widget == self.visible_child:
+                            if self.children_list:
+                                self.set_visible_child(self.children_list[0]) 
+                            else:
+                                self.visible_child = None
+            else:
+                raise NotInStackError(widget)
+
+    def set_visible_child(self, widget_or_identifier: BASECLASS | str) -> None: # type: ignore
+        """
+        sets visible the widget.
+        """
+        if isinstance(widget_or_identifier, BASECLASS):
+            widget = widget_or_identifier
+            if any(child["widget"] == widget for child in self.children_list):
+                if self.visible_child:
+                    self.visible_child.grid_remove()
+
+                widget.grid()
+                self.visible_child = widget
+            else:
+                raise NotInStackError(widget)
+        if isinstance(widget_or_identifier, str):
+            name = widget_or_identifier
+            if any(child["name"] == name for child in self.children_list):
+                for child in self.children_list:
+                    if child["name"] == name:
+                        if self.visible_child:
+                            self.visible_child.grid_remove()  # Hide the current visible widget
+
+                        widget = child["widget"]
+                        widget.grid()  # Show the widget associated with the name
+                        self.visible_child = widget
+            else:
+                raise NotInStackError(widget)
+
+    def get_visible_child(self) -> type(BASECLASS) | None: # type: ignore
+        """
+        gets the currently visible widget in the stack.
+        """ 
+        return self.visible_child
+    
+class NotInStackError(Exception):
+    def __init__(self, widget):
+        self.widget = widget
+        self.message = f"Widget: {widget} is not appart of the stack"
+    def __str__(self):
+        return self.message
--- test2.py
+++ test2.py
@@ -1,6 +1,16 @@
 import TkinterExtended as etk
 import customtkinter as ctk
 
+class Page_stack(etk.Stack):
+   def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.page1 = ctk.CTkFrame(self, bg_color="blue")
+        self.page2 = ctk.CTkFrame(self, bg_color="green")
+
+        self.add_widget(self.page1, "page1")
+        self.add_widget(self.page2, "page2")
+
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -11,16 +21,10 @@ class App(ctk.CTk):
         self.title_label = ctk.CTkLabel(self, text= "Test Application")
         self.title_label.grid(row = 0, column = 0)
 
-        self.stack = etk.Stack(self, bg_color = "red")
+        self.stack = Page_stack(self, width = 200, height = 200, bg_color = "red")
         self.stack.grid(row = 1, column = 0)
 
-        self.page1 = ctk.CTkFrame(self.stack, width=200, bg_color="blue")
-        self.page2 = ctk.CTkFrame(self.stack, width=300, bg_color="green")
-
-        self.stack.add_widget(self.page1, "page1")
-        self.stack.add_widget(self.page2, "page2")
-
-        self.page1_button = ctk.CTkButton(self, text="page 1", command=lambda: self.stack.set_visible_child(self.page1))
+        self.page1_button = ctk.CTkButton(self, text="page 1", command=lambda: self.stack.set_visible_child(self.stack.page1))
         self.page1_button.grid(row=2, column=0)
 
         self.page2_button = ctk.CTkButton(self, text="page 2", command=lambda: self.stack.set_visible_child("page2"))
