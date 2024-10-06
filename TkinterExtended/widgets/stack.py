f28bc934360855d2721099b987d19efc24c6e403
AJLocke1 <133682649+AJLocke1@users.noreply.github.com>
2024-10-06 23:45:41 +0100

    Added Callback system for 3 events: add widget, remove widget and changing the visible widget for the stack.

--- TkinterExtended/widgets/stack.py
+++ TkinterExtended/widgets/stack.py
@@ -1,4 +1,6 @@
 from TkinterExtended.exceptions import WidgetAlreadyInStackError, NotInStackError, DuplicateNameError
+import inspect
+import warnings
 
 try:
     import customtkinter as ctk
@@ -26,9 +28,13 @@ class Stack(STACKBASE):
     def __init__(self, *args, children_expandable = False, **kwargs):
         super().__init__(*args, bg_color = "transparent", fg_color = "transparent", **kwargs)
 
-        self.children_list = []
-        self.visible_child = None
-        self.children_expandable = children_expandable
+        self.children_list: list = [] #cant be a set as it needs to be ordered.
+        self.visible_child: BASECLASS = None # type: ignore
+        self.children_expandable: bool = children_expandable
+
+        self._on_add_widget_callbacks: set[callable] = set() 
+        self._on_remove_widget_callbacks: set[callable] = set()
+        self._on_change_visible_widget_callbacks: set[callable] = set()
 
         if self.children_expandable:
             self.grid_rowconfigure(0, weight=1)
@@ -79,6 +85,8 @@ class Stack(STACKBASE):
 
         if self.visible_child is None:  
             self.set_visible_child(widget)
+        
+        self._trigger_event_callbacks(event = "add_widget", widget = widget)
 
     def remove_widget(self, widget_or_identifier: BASECLASS | str | int) -> None: # type: ignore
         """
@@ -98,6 +106,8 @@ class Stack(STACKBASE):
         if isinstance(widget_or_identifier, int):
             self._remove_widget_by_index(widget_or_identifier)
 
+        self._trigger_event_callbacks(event = "remove_widget", widget = widget_or_identifier)
+
     def set_visible_child(self, widget_or_identifier: BASECLASS | str | int) -> None: # type: ignore
         """
             Based on the arguments provided it calls the necessary private set visible method to set a widget 
@@ -115,8 +125,10 @@ class Stack(STACKBASE):
 
         if isinstance(widget_or_identifier, int):
             self._set_visible_child_by_index(widget_or_identifier)
+        
+        self._trigger_event_callbacks(event = "change_visible_widget", widget = widget_or_identifier)
 
-    def get_visible_child(self) -> type(BASECLASS) | None: # type: ignore
+    def get_visible_child(self) -> BASECLASS | None: # type: ignore
         """
         gets the currently visible widget.
 
@@ -184,9 +196,9 @@ class Stack(STACKBASE):
             if child["widget"] == self.visible_child:
                 current_index = self.children_list.index(child)
         if current_index < len(self.children_list) -1:
-            self._set_visible_child_by_index(current_index+1)
+            self.set_visible_child(current_index+1)
         else:
-            self._set_visible_child_by_index(0)
+            self.set_visible_child(0)
 
     def show_previous(self) -> None:
         """
@@ -196,9 +208,9 @@ class Stack(STACKBASE):
             if child["widget"] == self.visible_child:
                 current_index = self.children_list.index(child)
         if current_index > 0:
-            self._set_visible_child_by_index(current_index-1)
+            self.set_visible_child(current_index-1)
         else:
-            self._set_visible_child_by_index(-1)
+            self.set_visible_child(-1)
 
     def _remove_widget_by_object(self, widget: BASECLASS): # type: ignore
         """
@@ -335,3 +347,121 @@ class Stack(STACKBASE):
         widget = child["widget"]
         widget.grid()  # Show the widget associated with the name
         self.visible_child = widget
+
+    def add_callback_function(self, event: str, callback_function: callable) -> None: 
+        """
+        Adds a callback function for the specified event. All callback function will automatically be passed
+        the event and widget effected in the key word arguments **kwargs. if this is not present in your funtion
+        these arguments will be discarded using a try-catch.
+
+        Args:
+            event (str): events are, add_widget, remove_widget and change_visible_widget.
+            callback_function (callable): the callback function to be added.
+
+        Raises:
+            TypeError: an error if the callback_function passed is not callable.
+            ValueError: if the event is invalid.
+        """
+        if callable(callback_function): 
+
+            signature = inspect.signature(callback_function)
+            parameters = signature.parameters
+
+            if ("event" not in parameters or "widget" not in parameters) and "**kwargs" not in parameters:
+                warnings.warn(f"Warning: The callback function {callback_function.__name__} does not accept 'event' "
+                                "and/or 'widget' parameters. These will not be passed to the function.")
+        
+            match event:
+                case "add_widget":
+                    self._on_add_widget_callbacks.add(callback_function)
+                case "remove_widget":
+                    self._on_remove_widget_callbacks.add(callback_function)
+                case "change_visible_widget":
+                    self._on_change_visible_widget_callbacks.add(callback_function)
+                case _:
+                    raise ValueError(f"{event} is an invalid event. valid events: add_widget, remove_widget, and change_visible_widget")
+        else:
+            raise TypeError(f"Object {callback_function} is not callable")
+        
+    def remove_callback_function(self, event: str, callback_function: callable) -> None: 
+        """
+        removes a callback function for the specified event.
+
+        Args:
+            event (str): events are, add_widget, remove_widget and change_visible_widget.
+            callback_function (callable): the callback function to be removed.
+
+        Raises:
+            TypeError: an error if the callback_function passed is not callable.
+            ValueError: if the event is invalid.
+        """
+        if callable(callback_function): 
+            match event:
+                case "add_widget":
+                    self._on_add_widget_callbacks.discard(callback_function)
+                case "remove_widget":
+                    self._on_remove_widget_callbacks.discard(callback_function)
+                case "change_visible_widget":
+                    self._on_change_visible_widget_callbacks.discard(callback_function)
+                case _:
+                    raise ValueError(f"{event} is an invalid event. valid events: add_widget, remove_widget, and change_visible_widget")
+        else:
+            raise TypeError(f"Object {callback_function} is not callable")
+    
+    def get_callback_functions(self, event: str) -> set[callable]:
+        """
+        gets the specified set of callback functions relating to the event.
+
+        Args:
+            event (str): events are, add_widget, remove_widget and change_visible_widget.
+
+        Raises:
+            ValueError: if the event is invalid.
+
+        Returns:
+            set[callable]: the set of callback functions.
+        """
+        match event:
+            case "add_widget":
+                return self._on_add_widget_callbacks
+            case "remove_widget":
+                return self._on_remove_widget_callbacks
+            case "change_visible_widget":
+                return self._on_change_visible_widget_callbacks
+            case _:
+                raise ValueError(f"{event} is an invalid event. valid events: add_widget, remove_widget, and change_visible_widget")
+        
+    def _trigger_event_callbacks(self, event: str, **kwargs):
+        """
+        private method that calls the callback functions when an event happens.
+
+        Args:
+            event (str): events are, add_widget, remove_widget and change_visible_widget.
+            **kwargs: Keyword arguments passed to the callback fucntions.
+
+        Raises:
+            TypeError: if the users function doesnt accept given keyword arguments it will recall it 
+            without them.
+
+        """
+        match event:
+            case "add_widget":
+                for callback in self._on_add_widget_callbacks:
+                    try:
+                        callback(**kwargs)
+                    except TypeError:
+                        callback()
+            case "remove_widget":
+                for callback in self._on_remove_widget_callbacks:
+                    try:
+                        callback(**kwargs)
+                    except TypeError:
+                        callback()
+            case "change_visible_widget":
+                for callback in self._on_change_visible_widget_callbacks:
+                    try:
+                        callback(**kwargs)
+                    except TypeError:
+                        callback()
+
+   
--- test.py
+++ test.py
@@ -16,6 +16,9 @@ class App(ctk.CTk):
         self.stack = etk.Stack(self, children_expandable=True)
         self.stack.grid(row = 1, column = 0, sticky="nsew")
 
+        self.stack.add_callback_function("add_widget", self.print_widget_added)
+        self.stack.add_callback_function("change_visible_widget", self.print_change_visible_widget)
+
         self.page1 = ctk.CTkFrame(self.stack, fg_color="blue")
         self.page2 = ctk.CTkFrame(self.stack, fg_color="green")
         self.page3 = ctk.CTkButton(self.stack, fg_color="orange",text="page 1", command=lambda: self.stack.set_visible_child(self.page1))
@@ -29,6 +32,12 @@ class App(ctk.CTk):
 
         self.next_button = ctk.CTkButton(self, text="next", command=lambda: self.stack.show_next())
         self.next_button.grid(row=0, column=2)
+
+    def print_widget_added(self, widget):
+        print("added")
+
+    def print_change_visible_widget(self, widget):
+        print("changed visible widget")
 
 if __name__ == "__main__":
     app = App()
